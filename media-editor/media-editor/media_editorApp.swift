304b265cfb430b8738ecaecbdfa6c0a548824239
Ibrahim3ziz <ichbinhima@gmail.com>
2024-03-04 02:51:30 +0200

    start project

---
+++ media-editor/media-editor/media_editorApp.swift
@@ -0,0 +1,17 @@
+//
+//  media_editorApp.swift
+//  media-editor
+//
+//  Created by Ibrahim Abdul Aziz on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct media_editorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
