5abb8af72e5e2c0b24a1f10ff505f4b343507896
Ibrahim3ziz <ichbinhima@gmail.com>
2024-03-20 01:28:03 +0200

    add colors

---
+++ media-editor/media-editor/Resources/Colors/Colors.swift
@@ -0,0 +1,116 @@
+//
+//  Colors.swift
+//  media-editor
+//
+//  Created by Ibrahim Abdul Aziz on 20/03/2024.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    /// 1ED291
+    public static var berbzaLight: Color {
+        return Color(red: 0.72, green: 0.00, blue: 0.00)
+    }
+    
+    /// #03BF7C
+    public static var berbzaDark: Color {
+        return Color(red: 0.01, green: 0.75, blue: 0.49)
+    }
+    
+    /// #03B072
+    public static var berbza: Color {
+        return Color(red: 0.01, green: 0.69, blue: 0.45)
+    }
+    
+    /// #FFF38B
+    public static var yellowLight: Color {
+        return Color(red: 1.00, green: 0.95, blue: 0.55)
+    }
+    
+    /// #FEEA38
+    public static var yellow: Color {
+        return Color(red: 1.00, green: 0.92, blue: 0.22)
+    }
+    
+    /// #EAD627
+    public static var redYellow: Color {
+        return Color(red: 0.92, green: 0.84, blue: 0.15)
+    }
+    
+    /// #AC7DFF
+    public static var purpleLight: Color {
+        return Color(red: 0.67, green: 0.49, blue: 1.00)
+    }
+    
+    /// #A06EFB
+    public static var purple: Color {
+        return Color(red: 0.63, green: 0.43, blue: 0.98)
+    }
+    
+    /// #8F54F9
+    public static var purpleDark: Color {
+        return Color(red: 0.56, green: 0.33, blue: 0.98)
+    }
+    
+    /// #FF7A92
+    public static var redLight: Color {
+        return Color(red: 1.00, green: 0.48, blue: 0.57)
+    }
+    
+    /// #FF5372
+    public static var red: Color {
+        return Color(red: 1.00, green: 0.33, blue: 0.45)
+    }
+    
+    /// #E8375
+    public static var redDark: Color {
+        return Color(red: 0.91, green: 0.22, blue: 0.34)
+    }
+    
+    /// #FFFFFF
+    public static var white: Color {
+        return Color(red: 1.00, green: 1.00, blue: 1.00)
+    }
+    
+    /// #252525
+    public static var gray: Color {
+        return Color(red: 0.15, green: 0.15, blue: 0.15)
+    }
+    
+    /// #000000
+    public static var black: Color {
+        return Color(red: 0.00, green: 0.00, blue: 0.00)
+    }
+    
+    /// #000000
+    public static var opacitybl3: Color {
+        return Color(red: 0.00, green: 0.00, blue: 0.00, opacity: 0.6)
+    }
+    
+    /// #000000
+    public static var opacitybl2: Color {
+        return Color(red: 0.00, green: 0.00, blue: 0.00, opacity: 0.3)
+    }
+    
+    /// #000000
+    public static var opacitybl1: Color {
+        return Color(red: 0.00, green: 0.00, blue: 0.00, opacity: 0.2)
+    }
+    
+    /// FFFFFF
+    public static var opacitywh3: Color {
+        return Color(red: 1.00, green: 1.00, blue: 1.00, opacity: 0.6)
+    }
+    
+    /// FFFFFF
+    public static var opacitywh2: Color {
+        return Color(red: 1.00, green: 1.00, blue: 1.00, opacity: 0.3)
+    }
+    
+    /// FFFFFF
+    public static var opacitywh1: Color {
+        return Color(red: 1.00, green: 1.00, blue: 1.00, opacity: 0.1)
+    }
+}
