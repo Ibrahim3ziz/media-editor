761ab8912b6159285c0ce522713464075abfc002
Ibrahim3ziz <ichbinhima@gmail.com>
2024-03-19 04:27:10 +0200

    add base button

--- media-editor/media-editor/Components/XButton/Base/ButtonSizes.swift
+++ media-editor/media-editor/Components/XButton/Base/ButtonSizes.swift
@@ -5,4 +5,16 @@
 //  Created by Ibrahim Abdul Aziz on 18/03/2024.
 //
 
-import Foundation
+import SwiftUI
+
+class ButtonBigSize: ButtonSizeProtocol {
+    var height: CGFloat = 52
+    var cornerRadiuse: CGFloat = 16
+    var font: Font = .system(size: 20)
+}
+
+class ButtonMediumSize: ButtonSizeProtocol {
+    var height: CGFloat = 44
+    var cornerRadiuse: CGFloat = 12
+    var font: Font = .system(size: 14)
+}
