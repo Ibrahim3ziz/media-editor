7d32f993749515cf734f58ff34f807e07a869677
Ibrahim3ziz <ichbinhima@gmail.com>
2024-03-19 04:25:09 +0200

    add fonts

--- media-editor/media-editor/Resources/Fonts/Fonts.swift
+++ media-editor/media-editor/Resources/Fonts/Fonts.swift
@@ -5,4 +5,61 @@
 //  Created by Ibrahim Abdul Aziz on 18/03/2024.
 //
 
-import Foundation
+import SwiftUI
+
+extension Font {
+    public static var superGreat: Font {
+        return .custom("Aleo-Regular_Black", size: 36)
+    }
+    
+    public static var great: Font {
+        return .custom("Aleo-Regular_ExtraBold", size: 24)
+    }
+    
+    public static var bodyBigBold: Font {
+        return .custom("Aleo-Regular_Bold", size: 20)
+    }
+    
+    public static var bodyBig: Font {
+        return .custom("Aleo-Regular_SemiBold", size: 20)
+    }
+    
+    public static var bodyBold: Font {
+        return .custom("Aleo-Regular_Medium", size: 16)
+    }
+    
+    public static var body: Font {
+        return .custom("Aleo-Regular", size: 16)
+    }
+    
+    public static var bodySmall: Font {
+        return .custom("Aleo-Regular_Light", size: 14)
+    }
+    
+    public static var small: Font {
+        return .custom("Aleo-Regular_ExtraLight", size: 12)
+    }
+}
+
+/*
+ /// Font Names
+ Aleo
+ ----- Aleo-Regular
+ ----- Aleo-Italic
+ ----- Aleo-Regular_Thin
+ ----- Aleo-Italic_Thin-Italic
+ ----- Aleo-Regular_ExtraLight
+ ----- Aleo-Italic_ExtraLight-Italic
+ ----- Aleo-Regular_Light
+ ----- Aleo-Italic_Light-Italic
+ ----- Aleo-Regular_Medium
+ ----- Aleo-Italic_Medium-Italic
+ ----- Aleo-Regular_SemiBold
+ ----- Aleo-Italic_SemiBold-Italic
+ ----- Aleo-Regular_Bold
+ ----- Aleo-Italic_Bold-Italic
+ ----- Aleo-Regular_ExtraBold
+ ----- Aleo-Italic_ExtraBold-Italic
+ ----- Aleo-Regular_Black
+ ----- Aleo-Italic_Black-Italic
+ */
