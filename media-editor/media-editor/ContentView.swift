8ecc353e4786a36079396e78c69020f00e66799f
Ibrahim3ziz <ichbinhima@gmail.com>
2024-03-20 03:46:54 +0200

    finish home without actions

--- media-editor/media-editor/ContentView.swift
+++ media-editor/media-editor/ContentView.swift
@@ -11,20 +11,26 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                
+                Color.blackHq.ignoresSafeArea()
                 Text("")
                     .navigationTitle("")
                     .toolbar {
                         ToolbarItem(placement: .topBarLeading) {
                             Image(systemName: "questionmark.circle")
+                                .foregroundStyle(Color.whiteHq)
                         }
                         
                         ToolbarItem(placement: .topBarTrailing) {
-                            XTextImageButton(title: "pro_title", image: Image(.homeEmptyState))
+                            XTextImageButton(title: "pro_title", image: Image(.rocket), backgroundColor: .redHq, foregroundColor: .whiteHq)
                         }
                     }
                 
-                EmptyStateView()
+                VStack(alignment: .center, spacing: 24, content: {
+                    EmptyStateView()
+                    
+                    XTextImageButton(title: "create_project", image: Image(systemName: "plus"), backgroundColor: .purpleHq, foregroundColor: .whiteHq)
+                })
+                .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16))
             }
         }
     }
